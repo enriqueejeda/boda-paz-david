8357a6ed5fe14dc640d42495c8df1fbc4ea0a3aa
Enrique Ejeda <enriqueejeda2016@gmail.com>
2026-01-18 17:45:08 +0100

    feat: Eliminar la paleta de colores 'wedding' del tema en la configuración de Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,24 +7,11 @@ export default {
     theme: {
         extend: {
             colors: {
-                wedding: {
-                    50: '#faf5f0',
-                    100: '#f5ebe0',
-                    200: '#ead6c1',
-                    300: '#dfc2a2',
-                    400: '#d4ad83',
-                    500: '#c99864',
-                    600: '#bd8345',
-                    700: '#b16e26',
-                    800: '#a5591f',
-                    900: '#994418',
-                },
             },
         },
     },
     plugins: [],
     corePlugins: {
-        // Desactiva plugins de Tailwind que no uses
         aspectRatio: true,
         backdropBlur: true,
         backdropBrightness: true,
